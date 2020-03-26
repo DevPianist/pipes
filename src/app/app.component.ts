112ececd2f58454cddfe91fe64c97ad34d8067ac
DevPianist <oscardelgadillodev@outlook.com>
2020-03-25 20:22:31 -0500

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = "Capitán América";
+  nombre2: string = "OscAR SandRo";
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  personajes: string[] = ["Ironman", "Spiderman", "Thor", "Loki", "Groot"];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+  fecha: Date = new Date();
+  activar: boolean = true;
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 40,
+    direccion: {
+      calle: "Primera",
+      casa: 10
+    }
+  };
+  idioma: string = "en";
+  cambiarIdioma(idioma: string) {
+    this.idioma = idioma;
+  }
+  videoUrl: string = "https://www.youtube.com/embed/O-rhXZLtpv0";
+  valorPromesa = new Promise<string>(resolve => {
+    setTimeout(() => {
+      resolve("data");
+    }, 4500);
+  });
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, LOCALE_ID } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+
+import { registerLocaleData } from "@angular/common";
+import localeEs from "@angular/common/locales/es";
+import localeFr from "@angular/common/locales/fr";
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+
+registerLocaleData(localeEs);
+registerLocaleData(localeFr);
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, CapitalizadoPipe, DomseguroPipe, ContrasenaPipe],
+  imports: [BrowserModule],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: "es"
+    }
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
